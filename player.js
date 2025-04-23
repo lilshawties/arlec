de41a0355e2251acb60c3c12ce7f626e4927ae64
bomie <bomidelrey@gmail.com>
2025-04-23 19:22:21 +0700

    player.js

--- player.js
+++ player.js
@@ -25,10 +25,10 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
         {
-      name: "Avinan",
-      password: "avinan",
-      host: "new-york-node-1.vortexcloud.xyz",
-      port:  5008,
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
       secure: false
         },
     ];
